a3351ecbdabe959f69e67785f10477705a580ce0
Victor Montalvão <vsmontalvao@gmail.com>
2012-11-10 20:01:45 -0200

    faltou titulo da classe

--- EverHack/src/com/example/everhack/EverHackGamesActivity.java
+++ EverHack/src/com/example/everhack/EverHackGamesActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class EverHackGamesActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
